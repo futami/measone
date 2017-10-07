abd70a419c769eb6e69475aa5634f5c3a501804b
ryu <futamir@gmail.com>
2017-10-07 14:16:38 +0900

    Add url link to template files

--- meas/urls.py
+++ meas/urls.py
@@ -7,15 +7,18 @@ router.register(r'entries', views.EntryViewSet)
 
 from django.conf.urls import url
 urlpatterns = [
-    url(r'^conditions/$', views.ConditionListView.as_view()),
+    url(r'^conditions/$', views.ConditionListView.as_view(), name='list'),
+    url(r'^condition/(?P<pk>\d+)$', views.ConditionDetailView.as_view(), name='detail'),
 ]
 
 # はじめての Django アプリ作成、その 3 | Django documentation | Django 
 # https://docs.djangoproject.com/ja/1.11/intro/tutorial03/
-urlpatterns += [
-    url(r'^$', views.index, name='index'),
-]
+#urlpatterns += [
+#    url(r'^$', views.index, name='index'),
+#]
 
 # はじめての Django アプリ作成、その 4 | Django documentation | Django 
 # https://docs.djangoproject.com/ja/1.11/intro/tutorial04/
-
+urlpatterns += [
+    url(r'^$', views.ConditionListView.as_view(), name='index'),
+]
--- meas/views.py
+++ meas/views.py
@@ -27,8 +27,24 @@ def index(request):
     latest_condition_list = Condition.objects.order_by('-created_at')
     output = ', '.join([c.description for c in latest_condition_list])
     return HttpResponse(output)
-    
+
 # はじめての Django アプリ作成、その 4 | Django documentation | Django 
 # https://docs.djangoproject.com/ja/1.11/intro/tutorial04/
+# genericを使ったListViewは <app>/<model>_list.html というdefaultのtemplateを使う
+# template_name で指定できる
+# ListViewは、自動的に生成されるコンテキスト変数は <model>_list になります。
+# context_object_name 属性を与えると指定できる 
+from django.views import generic
+class ConditionListView(generic.ListView):
+#    template_name = 'app_name/index.html'
+#    context_object_name = 'latest_question_list'
+    model = Condition
+#    def get_queryset(self):
+#        """Return the last five published questions."""
+#        return Condition.objects.order_by('-created_at')[:5]
 
+class ConditionDetailView(generic.DetailView):
+    model = Condition
+#    def get_queryset(self):
+#        return Condition.objects.filter(condition.id=pk)
 
